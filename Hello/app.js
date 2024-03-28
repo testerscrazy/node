b5e3b9239aaee7a49c368708ca01dc9b31c6d6c6
testerscrazy <165315196+testerscrazy@users.noreply.github.com>
2024-03-28 11:32:33 -0700

    Add files via upload

---
+++ Hello/app.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+  
+  // Example usage:
+  const originalString = "Hello, World!";
+  const reversedString = reverseString(originalString);
+  
+  console.log(reversedString); // Outputs: "!dlroW ,olleH"
